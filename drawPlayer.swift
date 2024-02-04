866534f13f5e09d9907c4f0e38c0bb316bcf7b4c
Contreras <152905146+zestkovaleksej@users.noreply.github.com>
2024-02-04 17:19:14 +0100

    Create drawPlayer.swift

---
+++ drawPlayer.swift
@@ -0,0 +1,10 @@
+    // Отрисовка игрока
+      drawPlayer();
+      // Рекурсивный вызов функции update для обновления игры
+      requestAnimationFrame(update);
+    }
+    // Начало игры
+    update();
+  </script>
+</body>
+</html>
